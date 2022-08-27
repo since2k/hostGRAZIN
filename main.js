8892a24900c6ece9e9f35595c2d58c279a1fe673
s2k <98129205+since2k@users.noreply.github.com>
2022-08-28 11:46:37 +1200

    Add files via upload
    whatuworried4?

---
+++ main.js
@@ -0,0 +1,30 @@
+var line = "meh";
+
+const meh = () => {
+    for (let i = 0; i < line.length(); i++){
+        console.log(line[i]);
+    }
+}
+
+line == "meh" ? console.log("Positive") : line === "meh";
+
+DeviceMotionEvent(); {
+    const counts = 0;
+    counts++;
+}
+
+FileSystemDirectoryEntry;
+
+const elastic = () => {
+    var space = Math.sin(Math.PI * Math.sin) / 25;
+    return space;
+}
+
+var velocity = 1.0;
+var speed = 1.0;
+
+velocity = speed ? velocity++ : speed++;
+
+document.contains = () => {
+    var sent = 5 + 2;
+}
